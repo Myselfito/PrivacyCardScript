fc2862db5bf868c820005d452b5545997515c30c
xheatedx <melgozas.92@gmail.com>
2018-04-18 11:06:33 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+sessionid = "21d412d1-3e32-492b-badb-1d83a0cb6f24"
+ctoken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1NzUyNTBlMTJmM2ZkMmQxMjY5OGQxMjUiLCJpYXQiOjE1MjQwNDc0ODMsImV4cCI6MTUyNDA2NTQ4M30.20dwpdwPEfv96pgtAEdACYvvg2BdeLJe55NhTCPWqY8"
+etag = "\"ps26i5unssI=\""
+card_names = ["014", "015", "016", "017", "018", "019", "020","021","022","023","024","025","026"]
+limit_duration = "MONTHLY"
+spend_limit = "300"
+MAXCARDS = 13
